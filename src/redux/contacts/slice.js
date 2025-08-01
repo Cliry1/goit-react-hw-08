5a725c2b7b4735d0f76e413699c861e076a60098
PRO100 Cliry <128536194+Cliry1@users.noreply.github.com>
2025-08-02 01:43:16 +0300

    update

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -35,7 +35,7 @@ const contactsSlice = createSlice({
     .addCase(deleteContacts.fulfilled, (state, action)=>{
       state.loading = false;
       state.error = null;
-      const index = state.items.findIndex((contact)=>{contact.id===action.payload.id})
+      const index = state.items.findIndex((contact)=>contact.id===action.payload.id)
       state.items.splice(index,1)
     })
     .addCase(deleteContacts.rejected, handleRejected)
